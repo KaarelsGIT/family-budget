26e81b2569df1a6550d06ca203b7113bc7f2e26d
kaarelkattai <kaarel.kattai@gmail.com>
2026-05-08 23:16:48 +0300

    fixed transfer error

--- src/main/java/ee/kaarel/familybudgetapplication/dto/transaction/CreateTransactionRequest.java
+++ src/main/java/ee/kaarel/familybudgetapplication/dto/transaction/CreateTransactionRequest.java
@@ -8,33 +8,28 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record CreateTransactionRequest(
-        @NotNull @DecimalMin(value = "0.01") BigDecimal amount,
+        @NotNull @DecimalMin(value = "0.01")
+        BigDecimal amount,
 
-        @NotNull TransactionType type,
+        @NotNull
+        TransactionType type,
 
         Long fromAccountId,
-
         Long toAccountId,
-
         Long targetUserId,
-
         Long categoryId,
 
-        @NotNull LocalDate transactionDate,
+        @NotNull
+        LocalDate transactionDate,
 
-        @Size(max = 500) String comment,
+        @Size(max = 500)
+        String comment,
 
         Long reminderId,
-
-        // 1. Muudetud 'boolean' -> 'Boolean' (suur täht), et lubada nulli vastuvõtmist
-        // 2. Võid lisada loogika, et kui on null, siis käsitletakse kui false
         Boolean useMicroSavings,
-
-        // 3. Multiplier on juba Integer (objekt), mis on hea
         Integer multiplier
 ) {
-    // See kompaktne konstruktor tagab, et isegi kui Angular saadab nulli,
-    // on klassi sees väärtused alati olemas (default väärtused).
+
     public CreateTransactionRequest {
         if (useMicroSavings == null) {
             useMicroSavings = false;
